994488db130db01dace557135c3960e233b4f4d0
Jose Pradenas Navarro <josepradenasnavarro@gmail.com>
2022-05-27 21:10:15 -0400

    exercise 8.16

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
-const { ApolloServer, gql, UserInputError } = require('apollo-server');
+const {
+  ApolloServer,
+  gql,
+  UserInputError,
+  AuthenticationError
+} = require('apollo-server');
 const mongoose = require('mongoose');
 const Book = require('./models/Book');
 const Author = require('./models/Author');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
 
+const JWT_SECRET = process.env.JWT_SECRET;
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose
   .connect(MONGODB_URI)
@@ -16,6 +24,7 @@ const typeDefs = gql`
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Author {
@@ -41,6 +50,18 @@ const typeDefs = gql`
       genres: [String!]!
     ): Book
     editAuthor(name: String!, setBornTo: Int!): Author
+    createUser(username: String!, favouriteGenre: String!): User
+    login(username: String!, password: String!): Token
+  }
+
+  type User {
+    username: String!
+    favouriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
   }
 `;
 
@@ -72,6 +93,9 @@ const resolvers = {
     },
     allAuthors: async () => {
       return await Author.find({});
+    },
+    me: (root, args, { currentUser }) => {
+      return currentUser;
     }
   },
   Author: {
@@ -80,7 +104,8 @@ const resolvers = {
     }
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) throw new AuthenticationError('not authenticated');
       const author = await Author.findOne({ name: args.author });
       let authorID;
       if (!author) {
@@ -100,7 +125,8 @@ const resolvers = {
       }
       return await book.populate('author');
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) throw new AuthenticationError('not authenticated');
       const author = await Author.findOne({ name: args.name });
       author.born = args.setBornTo;
       try {
@@ -109,13 +135,50 @@ const resolvers = {
         throw new UserInputError(error.message, { invalidArgs: args });
       }
       return author;
+    },
+    createUser: async (root, args) => {
+      const { username, favouriteGenre } = args;
+      const user = new User({ username, favouriteGenre });
+      try {
+        await user.save();
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        });
+      }
+      return user;
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+      if (!user || args.password !== 'secret') {
+        throw new UserInputError('wrong credentials');
+      }
+      const userForToken = {
+        username: user.username,
+        id: user._id // why no .toString() ?
+      };
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
     }
   }
 };
 
+const getUser = async (auth) => {
+  if (!auth) return null;
+  const isBearer = auth.toLowerCase().startsWith('bearer');
+  if (isBearer) {
+    const encodedToken = auth.substring(7);
+    const decodedToken = jwt.verify(encodedToken, JWT_SECRET);
+    const currentUser = await User.findById(decodedToken.id);
+    return currentUser;
+  }
+};
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    const currentUser = await getUser(req.headers.authorization);
+    return { currentUser };
+  }
 });
 
 server.listen().then(({ url }) => {
---
+++ library-backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  favouriteGenre: {
+    type: String
+  }
+});
+
+const User = mongoose.model('User', schema);
+module.exports = User;
