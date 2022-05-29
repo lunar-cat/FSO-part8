40b929cff3ac5eebbf7c1757ae62e98c2565ad64
Jose Pradenas Navarro <josepradenasnavarro@gmail.com>
2022-05-29 13:20:40 -0400

    exercise 8.18 - context with token

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-app-token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  };
+});
+
+const link = new HttpLink({ uri: 'http://localhost:4000' });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: authLink.concat(link),
   cache: new InMemoryCache()
 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
